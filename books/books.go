7f77a485bcffe86792a392dafa30eb23b4344335
Fiona <frcondon@gmail.com>
2018-05-15 15:07:32 -0400

    Write to google storage

--- books/books.go
+++ books/books.go
@@ -6,17 +6,18 @@ import (
   "fmt"
   "os"
   "regexp"
+
+	"cloud.google.com/go/storage"
+	"golang.org/x/net/context"
 )
+
+var ctx = context.Background()
 
 var bookEnd = regexp.MustCompile("^\\*\\*\\* END OF THIS PROJECT GUTENBERG .+ \\*\\*\\*$")
 var chapterHeading = regexp.MustCompile("^PART \\w+$")
 
-func chapterFile(bookId string, chapterInd int) (*os.File, error) {
-  return os.Create(fmt.Sprintf("data/%s_%d.txt", bookId, chapterInd))
-}
-
-func chapterizeBook(id string) error {
-	inFile, err := os.Open(fmt.Sprintf("data/%s.txt", id))
+func chapterizeBook(bookId string, bkt *storage.BucketHandle) error {
+	inFile, err := os.Open(fmt.Sprintf("data/%s.txt", bookId))
   defer inFile.Close()
   if err != nil {
     return err
@@ -24,24 +25,24 @@ func chapterizeBook(id string) error {
 
   scanner := bufio.NewScanner(inFile)
   chapterInd := 0
-  var outFile *os.File
+  var objWriter *storage.Writer
 
   for scanner.Scan() {
     line := scanner.Text()
     if bookEnd.MatchString(line) {
       return nil
     } else if chapterHeading.MatchString(line) {
-      if outFile != nil {
-        outFile.Close()
+      if objWriter != nil {
+        if err := objWriter.Close(); err != nil {
+          return err
+        }
       }
 
       chapterInd++
-      outFile, err = chapterFile(id, chapterInd)
-      if err != nil {
-        return err
-      }
-    } else if outFile != nil {
-      if _, err := outFile.Write([]byte(scanner.Text() + "\n")); err != nil {
+
+      objWriter = bkt.Object(fmt.Sprintf("bigf/data/%s_%d.txt", bookId, chapterInd)).NewWriter(ctx)
+    } else if objWriter != nil {
+      if _, err := objWriter.Write([]byte(scanner.Text() + "\n")); err != nil {
         return err
       }
     }
@@ -51,7 +52,14 @@ func chapterizeBook(id string) error {
 }
 
 func main() {
-  err := chapterizeBook("1399")
+  client, err := storage.NewClient(ctx)
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  bkt := client.Bucket("gutenbits.appspot.com")
+
+  err = chapterizeBook("1399", bkt)
   if err != nil {
     fmt.Println(err)
   }
